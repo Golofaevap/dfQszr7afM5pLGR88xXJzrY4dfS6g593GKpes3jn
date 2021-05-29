4ce5f0bba164efa6e96555b0505d44bdc6193867
Marina <golofaeva.xs4n@gmail.com>
2021-05-29 22:33:52 +0800

    console log added to test

--- pages/api/test.js
+++ pages/api/test.js
@@ -4,7 +4,7 @@ import account from "../../utils/models/accounts";
 import log from "../../utils/models/logs";
 
 const handler = async (req, res) => {
-    // console.log(req.body);
+    console.log(req.body);
     const log__ = new log({ log: JSON.stringify(req.body, null, 5) });
     await log__.save();
     const acc = new account({ name: "first" });
