93bb9a6aa9543d6f202d61937ca0e4757b1e2cb5
Marina <golofaeva.xs4n@gmail.com>
2021-05-29 18:28:57 +0800

    console log added to test

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,6 +3,7 @@ import connectDB from "../../utils/mongodb";
 import account from "../../utils/models/accounts";
 
 const handler = async (req, res) => {
+    console.log(req.body);
     const acc = new account({ name: "first" });
     await acc.save();
     return res.json({ ok: true });
