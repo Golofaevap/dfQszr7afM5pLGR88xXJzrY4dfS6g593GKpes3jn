6f80e5f8b9c751e6979850496ba487623cf926be
Marina <golofaeva.xs4n@gmail.com>
2021-05-29 22:58:26 +0800

    version 778

--- pages/api/test.js
+++ pages/api/test.js
@@ -4,12 +4,14 @@ import account from "../../utils/models/accounts";
 import log from "../../utils/models/logs";
 
 const handler = async (req, res) => {
+    console.log("req.body");
     console.log(req.body);
+    console.log(req);
     // const log__ = new log({ log: JSON.stringify(req.body, null, 5) });
     // await log__.save();
-    const acc = new account({ name: "first" });
-    await acc.save();
-    return res.json({ ok: true, version: 777 });
+    // const acc = new account({ name: "first" });
+    // await acc.save();
+    return res.json({ ok: true, version: 778 });
 };
 
 export default connectDB(handler);
